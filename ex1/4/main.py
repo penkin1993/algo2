a3b809cf4db315c991aacaf328e724dff1ff45ff
Григорий Пенкин <penkin.g@yandex.ru>
2020-02-28 02:14:00 +0300

    ex

--- ex1/4/main.py
+++ ex1/4/main.py
@@ -2,17 +2,33 @@ import sys
 
 
 def get_metric(s1: str, s2: str):
-    if not s1:
-        return len(s2)
+    n = len(s1) + 1
+    m = len(s2) + 1
 
-    if not s2:
-        return len(s1)
+    answer_matrix = []
+    for _ in range(n):
+        answer_matrix.append(m * [0])
 
-    eq_operation = get_metric(s1[1:], s2[1:]) + int(not(s1[0] == s2[0]))
-    sub_operation_1 = get_metric(s1[1:], s2) + 1
-    sub_operation_2 = get_metric(s1, s2[1:]) + 1
+    for i in range(n):
+        for j in range(m):
 
-    return min(eq_operation, sub_operation_1, sub_operation_2)
+            if i == 0:
+                answer_matrix[i][j] = j
+
+            elif j == 0:
+                answer_matrix[i][j] = i
+
+            elif s1[i - 1] == s2[j - 1]:
+                answer_matrix[i][j] = answer_matrix[i - 1][j - 1]
+
+            else:
+                answer_matrix[i][j] = 1 + min(answer_matrix[i - 1][j - 1],
+                                              answer_matrix[i - 1][j],
+                                              answer_matrix[i][j - 1])
+
+    # print(answer_matrix)
+
+    return answer_matrix[-1][-1]
 
 
 s1 = sys.stdin.readline()[:-1]
