00a26f39834354ed98128f431e719062dc449626
Григорий Пенкин <penkin.g@yandex.ru>
2020-03-08 17:26:23 +0300

    err 3-

--- ex2/3/main.py
+++ ex2/3/main.py
@@ -5,8 +5,8 @@ from collections import defaultdict
 def dfs(top, dp_0, dp_1):
     [dfs(child, dp_0, dp_1) for child in graph_structure[top]]
 
-    dp_0[top] = 0 + sum([dp_0[child] for child in graph_structure[top]])
-    dp_1[top] = 1 + sum([dp_1[child] for child in graph_structure[top]])
+    dp_0[top] += sum([dp_1[child] for child in graph_structure[top]])
+    dp_1[top] += sum([dp_0[child] for child in graph_structure[top]])
 
     return max(dp_0[0], dp_1[0])
 
@@ -14,21 +14,21 @@ def dfs(top, dp_0, dp_1):
 n = int(sys.stdin.readline())
 graph_structure = defaultdict(list)
 
-sys.stdin.readline()  # пропуск нуля
-
-for i in range(n - 1):
+for i in range(1, n + 1):
     edge = int(sys.stdin.readline())
-    graph_structure[edge - 1].append(i + 1)
+    if edge != 0:
+        graph_structure[edge].append(i)
+    else:
+        root = i
 
-print(graph_structure)
+# print(graph_structure)
 
 dp_0 = defaultdict(lambda: 0)  # если не храним текущую врешину
 dp_1 = defaultdict(lambda: 1)  # если храним текущую врешину
 
-print(dfs(0, dp_0, dp_1))
+dfs(root, dp_0, dp_1)
 
-print(dp_0)
-print(dp_1)
+print(max(dp_0[root], dp_1[root]))
 
 """
 5
@@ -37,5 +37,15 @@ print(dp_1)
 1
 2
 3
+
+"""
+"""
+6
+5
+6
+5
+1
+0
+1
 
 """
