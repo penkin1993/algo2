a538afc98b816c8261f4bf222786b5afaa644d34
Григорий Пенкин <penkin.g@yandex.ru>
2020-03-12 12:14:36 +0300

    1+

--- ex3/1_/main.cpp
+++ ex3/1_/main.cpp
@@ -5,12 +5,11 @@ void print_multiplications(int number, std::vector<int> *min_del) {
     while (number != 1) {
         std::cout << min_del->at(number - 2) << " ";
         number = number / min_del->at(number - 2);
-
     }
     std::cout << "\n";
 }
 
-void get_min_del(std::vector<int> *min_del){
+void get_min_del(std::vector<int> *min_del) {
     for (int i = 2; i < min_del->size(); i++) {
         if (min_del->at(i - 2) == 0) {
             for (int j = i; j < min_del->size(); j += i) {
@@ -22,12 +21,11 @@ void get_min_del(std::vector<int> *min_del){
     }
 }
 
-
 int main() {
     std::ios::sync_with_stdio(false), std::cin.tie(0), std::cout.tie(0);
     int n, number;
     std::cin >> n;
-    std::vector<int> min_del(1000000, 0);
+    std::vector<int> min_del(1000001, 0);
     std::vector<int> prime_numbers;
     get_min_del(&min_del);
 
@@ -37,13 +35,3 @@ int main() {
     }
     return 0;
 }
-
-/*
-4
-60
-14
-3
-55
-
-*/
-
