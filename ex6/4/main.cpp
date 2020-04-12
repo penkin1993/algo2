c4b76d35318463c0778782b4c10587febc2e6f90
Григорий Пенкин <penkin.g@yandex.ru>
2020-04-13 01:56:02 +0300

    fix

--- ex6/4/main.cpp
+++ ex6/4/main.cpp
@@ -6,8 +6,8 @@ struct TreeNode {
     explicit TreeNode(int_fast64_t _parent) : parent(_parent) {}
 
     int_fast64_t parent;
-    int_fast64_t depth = - 1;
-    std::vector<int_fast64_t> children;
+    int_fast64_t depth = -1;
+    //std::vector<int_fast64_t> children;
     std::vector<int_fast64_t> jmp;
 };
 
@@ -25,30 +25,52 @@ private:
 
 void Tree::add(int_fast64_t node_id) {
     nodes.emplace_back(node_id - 1); // добавили новую врешину
-    nodes.back().parent = nodes[node_id -1].depth + 1; // dfs
-    nodes[node_id - 1].children.push_back(nodes.size() - 1); // сообщили родителю о новом ребенке
+    nodes.back().parent = nodes[node_id - 1].depth + 1; // dfs
+    //nodes[node_id - 1].children.push_back(nodes.size() - 1); // сообщили родителю о новом ребенке
 }
 
 
 void Tree::pre_calc() {
-    for (auto & node : nodes) {
+    for (auto &node : nodes) {
         node.jmp.push_back(node.parent);
     }
     for (int_fast64_t k = 1; k < std::ceil(log(nodes.size())) + 1; k++) {
-        for (auto & node : nodes) {
-            node.jmp.push_back(nodes[node.jmp[k-1]].jmp[k-1]);
+        for (auto &node : nodes) {
+            node.jmp.push_back(nodes[node.jmp[k - 1]].jmp[k - 1]);
         }
     }
 }
 
-int_fast64_t get_lca(int_fast64_t x, int_fast64_t y){
+int_fast64_t Tree::get_lca(int_fast64_t x, int_fast64_t y) {
+    x -= 1;
+    y -= 1;
+    if (nodes[x].depth < nodes[y].depth) {
+        std::swap(x, y);
+    }
+    int_fast64_t delta = x - y;
 
 
+    for (int_fast64_t k = std::ceil(log(nodes.size())); k > 0; k--) { // поднялись на одинаковую глубину
+        if (delta >= 1 << k) {
+            x = nodes[x].jmp[k];
+            delta -= 1 << k;
+        }
+    }
+    if (x == y) {
+        return x;
+    }
 
-
+    int_fast64_t _x, _y;
+    for (int_fast64_t k = std::ceil(log(nodes.size())); k > 0; k--) {
+        _x = nodes[x].jmp[k];
+        _y = nodes[y].jmp[k];
+        if (_x != y){
+            x = _x;
+            y = _y;
+        }
+    }
+    return nodes[x].jmp[0];
 }
-
-
 
 
 int main() {
@@ -71,3 +93,34 @@ int main() {
     }
     return 0;
 }
+
+/*
+5
+1
+1
+2
+3
+2
+2 3
+4 5
+
+1
+1
+
+
+
+5
+1
+1
+2
+2
+3
+4 5
+4 2
+3 5
+
+2
+2
+1
+
+*/
