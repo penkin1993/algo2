5ec63175ea9b84e42783be4f16a0590b188126c1
Григорий Пенкин <penkin.g@yandex.ru>
2020-04-14 00:56:21 +0300

    3+

--- ex6/5/main.cpp
+++ ex6/5/main.cpp
@@ -11,15 +11,16 @@ public:
 
     void pre_calc();
 
-    int_fast64_t get_lca(int_fast64_t x, int_fast64_t y);
-
     int_fast64_t get_min_weight(int_fast64_t x, int_fast64_t y);
 
 private:
     std::vector<int_fast64_t> parent = {0};
     std::vector<int_fast64_t> depth = {0};
-    std::vector<std::vector<int_fast64_t>> jmp;
     std::vector<int_fast64_t> weights = {0};
+
+    std::vector<std::vector<int_fast64_t>> jmp;
+    std::vector<std::vector<int_fast64_t>> w_jmp;
+
 };
 
 void Tree::add(int_fast64_t node_id, int_fast64_t weight) {
@@ -33,62 +34,60 @@ void Tree::pre_calc() {
     for (int_fast64_t k = 0; k < parent.size(); k++) {
         jmp.emplace_back();
         jmp[k].push_back(parent[k]);
+
+        w_jmp.emplace_back();
+        w_jmp[k].push_back(weights[k]);
     }
 
     for (int_fast64_t k = 1; k < std::ceil(log2(parent.size())) + 1; k++) {
         for (int_fast64_t v = 0; v < parent.size(); v++) {
             jmp[v].push_back(jmp[jmp[v][k - 1]][k - 1]);
+            w_jmp[v].push_back(std::min(w_jmp[jmp[v][k - 1]][k - 1], w_jmp[v][k - 1]));
         }
     }
 }
 
-int_fast64_t Tree::get_lca(int_fast64_t x, int_fast64_t y) {
+int_fast64_t Tree::get_min_weight(int_fast64_t x, int_fast64_t y) {
+    x -= 1;
+    y -= 1;
     if (depth[x] < depth[y]) {
         std::swap(x, y);
     }
+
+    int_fast64_t min_weight = INF;
     int_fast64_t delta = depth[x] - depth[y];
 
     for (int_fast64_t k = std::ceil(log2(parent.size())); k >= 0; k--) { // поднялись на одинаковую глубину
         if (delta >= 1 << k) {
+            if (min_weight > w_jmp[x][k]) {
+                min_weight = w_jmp[x][k];
+            }
             x = jmp[x][k];
             delta -= 1 << k;
         }
     }
 
     if (x == y) {
-        return x;
+        return min_weight;
     }
+
     int_fast64_t _x, _y;
     for (int_fast64_t k = std::ceil(log2(parent.size())); k >= 0; k--) {
         _x = jmp[x][k];
         _y = jmp[y][k];
         if (_x != _y) {
+            if (min_weight > w_jmp[x][k]) {
+                min_weight = w_jmp[x][k];
+            }
+            if (min_weight > w_jmp[y][k]) {
+                min_weight = w_jmp[y][k];
+            }
             x = _x;
             y = _y;
         }
     }
-    return jmp[x][0];
-}
-
-int_fast64_t Tree::get_min_weight(int_fast64_t x, int_fast64_t y){
-    x -= 1;
-    y -= 1;
-    int_fast64_t lca_node = get_lca(x, y);
-    int_fast64_t min_weight = INF;
-    while (x != lca_node) {
-        if (weights[x] < min_weight){
-            min_weight = weights[x];
-        }
-        x = parent[x];
-    }
-
-    while (y != lca_node) {
-        if (weights[y] < min_weight){
-            min_weight = weights[y];
-        }
-        y = parent[y];
-    }
-
+    min_weight = std::min(min_weight, w_jmp[x][0]);
+    min_weight = std::min(min_weight, w_jmp[y][0]);
     return min_weight;
 }
 
