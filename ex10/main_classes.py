d79543b61f39985f37b94c31a7f3363c75b3baed
penkin <penkin.g@yandex.ru>
2020-05-16 18:43:35 +0300

    10_fix

--- ex10/main_classes.py
+++ ex10/main_classes.py
@@ -1,6 +1,6 @@
 import operator
 
-from pyparsing import Literal, Word, Group, ZeroOrMore, Forward, nums, alphas, Regex, ParseException
+from pyparsing import Literal, Group, ZeroOrMore, Forward, nums, ParseException, Optional, Suppress, Word
 
 
 def bnf():
@@ -13,25 +13,26 @@ def bnf():
             else:
                 break
 
-    fnumber = Regex(r"[+-]?\d+(:?\.\d*)?(:?[eE][+-]?\d+)?")
-    ident = Word(alphas, alphas + nums + "_$")  # можно проще ?
+    p_m = Suppress("+") | Literal("-")
+    int_num = Word(nums)
+    float_num = Optional(p_m) + int_num + Optional('.' + int_num) + Optional('e' + Optional(p_m) + int_num)
 
-    plus = Literal("+")
-    minus = Literal("-")
+    add = Literal("+")
+    sub = Literal("-")
 
-    mult = Literal("*")
+    mul = Literal("*")
     div = Literal("/")
 
     lpar = Literal("(")
     rpar = Literal(")")
 
     expr = Forward()
-    atom = ((0, None) * minus + (ident | fnumber | ident + lpar + expr + rpar).
-            setParseAction(lambda x: expr_stack.append(x[0])) |
-            Group(lpar + expr + rpar)).setParseAction(push_uminus)  # шаг парсинга в рекурсии
 
-    term = atom + ZeroOrMore(((mult | div) + atom).setParseAction(lambda x: expr_stack.append(x[0])))
-    expr << term + ZeroOrMore(((plus | minus) + term).setParseAction(lambda x: expr_stack.append(x[0])))
+    atom = (ZeroOrMore(p_m) + ((float_num | nums + lpar + expr + rpar).setParseAction(lambda x: expr_stack.append(x[0])) | Group(lpar + expr + rpar)))\
+        .setParseAction(push_uminus)
+
+    term = atom + ZeroOrMore(((mul | div) + atom).setParseAction(lambda x: expr_stack.append(x[0])))
+    expr << term + ZeroOrMore(((add | sub) + term).setParseAction(lambda x: expr_stack.append(x[0])))
 
     return expr, expr_stack
 
@@ -59,32 +60,86 @@ def evaluate_stack(s):  # оформить статическим методом
 
 if __name__ == "__main__":  # поменять на классические юнитесты !!!
 
-    def test(s, expVal):
+    def test(s, exp_val):
         try:
             results, expr_stack = bnf()
-            results = results.parseString(s, parseAll=True)
-            # print("exprStack", exprStack)
+            # print("expr_stack", expr_stack)
             # print("results", results)
+            # print(expr_stack == [])
+            # expr_stack = []
+            results = results.parseString(s, parseAll=True)
             val = evaluate_stack(expr_stack[:])
         except ParseException as e:
             print(s, "failed parse:", str(e))
         except Exception as e:
             print(s, "failed eval:", str(e))
         else:
-            if val == expVal:
+            if val == exp_val:
                 print(s, "=", val, results, "=>", expr_stack)
             else:
-                print(s + "!!!", val, "!=", expVal, results, "=>", expr_stack)
+                print(s + "!!!", val, "!=", exp_val, results, "=>", expr_stack)
 
 
-    test("9", 9)
-    test("-9", -9)
-    test("--9", 9)
+    test("++9", 9)
+    test("+-9", -9)
+    test("----9", 9)
     test("9 + 3 + 6", 9 + 3 + 6)
     test("(9 + 3) / 11", (9 + 3.0) / 11)
     test("(9+3) / 11", (9 + 3.0) / 11)
-    test("(9 + 3)", (9 + 3))
+    test("((9 + 3))", (9 + 3))
+    test("-(9 + 3)", -(9 + 3))
     test("9 - 12 - 6", 9 - 12 - 6)
     test("9 - (12 - 6)", 9 - (12 - 6))
     test("2*3.14159", 2 * 3.14159)
     test("9 * (12 - 6)", 9 * (12 - 6))
+
+
+from pyparsing import Word, Suppress, alphas, nums
+
+
+class EqParse:
+    def __init__(self):
+        self.__var_dict = dict()
+        self.__expr, self.__expr_stack = self.bnf()
+
+    @staticmethod
+    def __eq_split(input_str: str):
+        """
+        Parse initial expression.
+        Parameters
+        ----------
+        input_str
+
+        Returns
+        -------
+        output is variable name and expression
+        """
+        greet = Word(alphas) + Suppress("=") + Word(nums + alphas + "+-*/()")
+        greeting = greet.parseString(input_str)
+        return greeting[0], greeting[1]
+
+    def __call__(self, input_str: str):
+        """
+        Parameters
+        ----------
+        input_str
+
+        Returns
+        -------
+
+        """
+        var, expr = self.__eq_split(input_str)
+        self.__var_dict[var] = None
+
+
+
+
+
+
+
+
+
+
+
+
+
