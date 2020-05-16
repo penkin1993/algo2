76ecab47e2160e2a59434e44cbc6bda2db2dbed6
penkin <penkin.g@yandex.ru>
2020-05-16 22:25:32 +0300

    10_ref_bnf

--- ex10/main_classes.py
+++ ex10/main_classes.py
@@ -8,6 +8,7 @@ class EqParse:
                            "-": operator.sub,
                            "*": operator.mul,
                            "/": operator.truediv}
+        self.__parser = self.__bnf()
         self.__expr_list = None
 
     def __parse_minus(self, x):
@@ -41,7 +42,7 @@ class EqParse:
         greeting = greet.parseString(input_str)
         return greeting[0], greeting[1]
 
-    def bnf(self):
+    def __bnf(self):
         """
         Returns
         -------
@@ -63,19 +64,28 @@ class EqParse:
                                ps.Optional('e' + ps.Optional(p_m) + int_num))
 
         str_num = ps.Combine(ps.Optional(p_m) + ps.Word(ps.alphas))
-        expr = ps.Forward()
+        parser = ps.Forward()
         parse_expr = (ps.ZeroOrMore(p_m) + (str_num.setParseAction(lambda x: self.__expr_list.append(x[0])) |
                                             float_num.setParseAction(lambda x: self.__expr_list.append(x[0])) |
-                                            (lpar + expr + rpar))).setParseAction(self.__parse_minus)
+                                            (lpar + parser + rpar))).setParseAction(self.__parse_minus)
 
         sum_expr = parse_expr + ps.ZeroOrMore(((mul | div) + parse_expr)
                                               .setParseAction(lambda x: self.__expr_list.append(x[0])))
 
-        expr << sum_expr + ps.ZeroOrMore(((add | sub) + sum_expr)
-                                         .setParseAction(lambda x: self.__expr_list.append(x[0])))
-        return expr
+        parser << sum_expr + ps.ZeroOrMore(((add | sub) + sum_expr)
+                                           .setParseAction(lambda x: self.__expr_list.append(x[0])))
+        return parser
 
-    def evaluate_stack(self, res_list):  # оформить статическим методом
+    def evaluate_stack(self, res_list):
+        """
+        Parameters
+        ----------
+        res_list
+
+        Returns
+        -------
+
+        """
         operation = res_list.pop()
         if operation == "minus":
             return -self.evaluate_stack(res_list)
@@ -103,9 +113,10 @@ class EqParse:
         """
         var, expr = self.__eq_split(input_str)
         self.__expr_list = []
-        result = self.bnf()
-        result.parseString(expr, parseAll=True)
+
+        self.__parser.parseString(expr, parseAll=True)
         val = self.evaluate_stack(self.__expr_list)
+
         self.__var_dict[var] = val
         print(expr, "=", val, correct_val == val)
 
