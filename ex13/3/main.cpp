679d6e37e3279ad5d9788de5183551a31a3a31eb
Your Name <you@example.com>
2020-06-12 17:40:46 +0300

    13-3

--- ex13/3/main.cpp
+++ ex13/3/main.cpp
@@ -1,34 +1,34 @@
 #include <iostream>
-#include <iomanip>
 #include <queue>
 #include <utility>
+#include <cmath>
 
-double F_INF = 101;
-double EPS = 1e-8;
-double SQUARE_LIM = 1e-8;
+long double F_INF = 101;
+long double F_INF_LIM = 100;
+long double EPS = 1e-13;
+long double SQUARE_LIM = 1e-8;
 
-struct Point;
 
 struct Line {
-    double a{}, b{}, c{};
+    long double a{}, b{}, c{};
 
-    explicit Line(double a_ = 0, double b_ = 0, double c_ = 0) : a(a_), b(b_), c(c_) {}
+    explicit Line(long double a_ = 0, long double b_ = 0, long double c_ = 0) : a(a_), b(b_), c(c_) {}
 };
 
 struct Point {
-    double x, y;
+    long double x, y;
 
-    explicit Point(double x_ = 0, double y_ = 0) : x(x_), y(y_) {}
+    explicit Point(long double x_ = 0, long double y_ = 0) : x(x_), y(y_) {}
 
     bool is_on_the_line(Line line_) const {
-        return std::abs(line_.a * x + line_.b * y + line_.c) < EPS;
+        return std::fabs(line_.a * x + line_.b * y + line_.c) < EPS;
     }
 
     bool intersect_sign(Line line_) const {
         return line_.a * x + line_.b * y + line_.c >= EPS;
     }
 
-    double vector_prod(Point point_) const {
+    long double vector_prod(Point point_) const {
         return x * point_.y - y * point_.x;
     }
 };
@@ -51,32 +51,22 @@ public:
 
     bool is_intersect(Line line_) const {
         // функция персечения двух отрезков
-        double sign0 = line_.a * point0.x + line_.b * point0.y + line_.c;
-        double sign1 = line_.a * point1.x + line_.b * point1.y + line_.c;
+        long double sign0 = line_.a * point0.x + line_.b * point0.y + line_.c;
+        long double sign1 = line_.a * point1.x + line_.b * point1.y + line_.c;
 
         return ((sign0 >= EPS) && (sign1 <= -EPS)) | ((sign0 <= -EPS) && (sign1 >= EPS));
     }
 
-    // bool is_on_the_line(Line line_) const{
-    //    return std::abs(line_.a * x + line_.b * y + line_.c) < EPS;
-    //}
-
-    // bool intersect_sign(Line line_) const {
-    //    return line_.a * x + line_.b * y + line_.c >= EPS;
-    //}
-
-
     Point intersection(Line line_) const {
         if (!is_intersect(line_)) {
-
             throw std::invalid_argument("Received wrong value");
         }
         // решаем уравнение
-        double d = line.a * line_.b - line.b * line_.a;
+        long double d = line.a * line_.b - line.b * line_.a;
 
-        double dx = line.b * line_.c - line_.b * line.c;
+        long double dx = line.b * line_.c - line_.b * line.c;
 
-        double dy = line.c * line_.a - line_.c * line.a;
+        long double dy = line.c * line_.a - line_.c * line.a;
 
         return Point(dx / d, dy / d);
     }
@@ -89,14 +79,13 @@ public:
 
     explicit Figure(std::queue<Point> points_);
 
-    void split(std::vector<Figure> *figure_array, double a, double b, double c); // алгоритм сечения фигуры прямой
+    bool split(std::queue<Figure> *figure_queue, long double a, long double b, long double c); // алгоритм сечения фигуры прямой
 
-    double calc_square(); // вычисление площади получивашейся фигуры
+    long double calc_square(); // вычисление площади получивашейся фигуры
 
     std::queue<Point> points;
 
 private:
-
 
     static bool check_lim(Point point);
 };
@@ -113,10 +102,10 @@ Figure::Figure(std::queue<Point> points_) {
 }
 
 bool Figure::check_lim(Point point) {
-    return (std::abs(point.x) == F_INF) | (std::abs(point.y) == F_INF);
+    return (std::fabs(point.x) >= F_INF_LIM) | (std::fabs(point.y) >= F_INF_LIM);
 }
 
-void Figure::split(std::vector<Figure> *figure_array, double a, double b, double c) {
+bool Figure::split(std::queue<Figure> *figure_queue, long double a, long double b, long double c) {
     Line new_line = Line(a, b, c);
 
     std::queue<Point> new_points;
@@ -136,25 +125,16 @@ void Figure::split(std::vector<Figure> *figure_array, double a, double b, double
         if (next_point1.is_on_the_line(new_line)) { // точка пересечения совпадает
             points.push(next_point1);
             new_points.push(next_point1);
+
         } else if ((next_point0.intersect_sign(new_line) || next_point0.is_on_the_line(new_line)) &
                    next_point1.intersect_sign(new_line)) {
-            /*
-            if (next_point0.is_on_the_line(new_line)) {
-                std::cout << " a " << a << " b " << b << " c " << c << "\n";
-                std::cout << " next_point0 " << next_point0.x << " " << next_point0.y << " next_point1 "
-                          << next_point1.x << " " << next_point1.y << "\n";
-            }*/
-
-
-
-
             points.push(next_point1);
+
         } else if ((!next_point0.intersect_sign(new_line) || next_point0.is_on_the_line(new_line)) &
                    !next_point1.intersect_sign(new_line)) {
-            // std::cout << " a " << a << " b " << b << " c " << c << "\n";
-            // std::cout << " next_point0 " << next_point0.x << " " << next_point0.y  << " next_point1 "  << next_point1.x << " " << next_point1.y << "\n";
 
             new_points.push(next_point1);
+
         } else {
             Point new_point = Segment(next_point0, next_point1).intersection(new_line);
             points.push(new_point);
@@ -170,15 +150,13 @@ void Figure::split(std::vector<Figure> *figure_array, double a, double b, double
     }
 
     if (new_points.size() > 2) {
-        figure_array->push_back(Figure(new_points));
-    } // касания исключаются
+        figure_queue->push(Figure(new_points));
+    }
+    return points.size() > 2;
 }
 
-double Figure::calc_square() {
-    if (points.size() < 3) {
-        return 0;
-    }
-    double square = 0;
+long double Figure::calc_square() {
+    long double square = 0;
 
     Point first_point = points.front();
     if (check_lim(first_point)) {
@@ -204,7 +182,7 @@ double Figure::calc_square() {
     }
     square += 0.5 * next_point0.vector_prod(first_point);
 
-    if (square < SQUARE_LIM) {
+    if (square <= SQUARE_LIM) {
         square = 0;
     }
 
@@ -213,59 +191,57 @@ double Figure::calc_square() {
 
 
 Line get_line(Point point0_, Point point1_) {
-    double a = point0_.y - point1_.y;
-    double b = point1_.x - point0_.x;
-    double c = point0_.x * point1_.y - point1_.x * point0_.y;
+    long double a = point0_.y - point1_.y;
+    long double b = point1_.x - point0_.x;
+    long double c = point0_.x * point1_.y - point1_.x * point0_.y;
 
     return Line(a, b, c);
 }
 
 int main() {
     std::ios::sync_with_stdio(false), std::cin.tie(0), std::cout.tie(0);
-    std::cout << std::fixed;
-    std::cout << std::setprecision(10);
+    // std::cout << std::fixed;
+    // std::cout << std::setprecision(10);
 
     int_fast32_t n;
     std::cin >> n;
 
-    Figure figure = Figure();
+    std::queue<Figure> figure_queue;
 
-    std::vector<Figure> figure_array;
+    figure_queue.push(Figure());
 
-    figure_array.push_back(figure);
-
-    double x0, y0, x1, y1;
+    long double x0, y0, x1, y1;
 
     for (int_fast32_t i = 0; i < n; i++) {
-
         std::cin >> x0 >> y0 >> x1 >> y1;
 
         Line line = get_line(Point(x0, y0), Point(x1, y1));
-
-        int_fast32_t array_size = figure_array.size();
+        int_fast32_t array_size = figure_queue.size();
         for (int_fast32_t j = 0; j < array_size; j++) {
 
-            // std::cout << line.a << " " << line.b << " " << line.c << "\n";
-            figure_array[j].split(&figure_array, line.a, line.b, line.c);
+            Figure figure = figure_queue.front();
+            figure_queue.pop();
+
+            if (figure.split(&figure_queue, line.a, line.b, line.c)) {
+                figure_queue.push(figure);
+            }
         }
     }
-    /*
-    std::cout << figure_array.size() << "\n";
-    for (auto & i : figure_array) {
-        std::cout << i.points.size() << " ";
-    }
-     */
-    std::vector<double> square_array;
 
-    for (auto &i : figure_array) {
-        double next_square = i.calc_square();
+    std::vector<long double> square_array;
+
+    while (!figure_queue.empty()) {
+        Figure figure = figure_queue.front();
+        figure_queue.pop();
+
+        long double next_square = figure.calc_square();
         if (next_square != 0) {
             square_array.push_back(next_square);
         }
     }
     std::cout << square_array.size() << "\n";
 
-    for (double i : square_array) {
+    for (long double i : square_array) {
         std::cout << i << "\n";
     }
     return 0;
@@ -279,10 +255,4 @@ int main() {
 0 1 0 0
 0 0 1 1
 
-
-2
-4
-6
-9
-12 != 15
 */
--- ex13/5/main.cpp
+++ ex13/5/main.cpp
@@ -1,6 +1,54 @@
 #include <iostream>
+#include <queue>
+#include <utility>
+#include <cmath>
+
+struct Point {
+    long double x, y;
+
+    explicit Point(long double x_ = 0, long double y_ = 0) : x(x_), y(y_) {}
+
+};
+
+struct Line {
+    long double a{}, b{}, c{};
+
+    explicit Line(long double a_ = 0, long double b_ = 0, long double c_ = 0) : a(a_), b(b_), c(c_) {}
+};
+
+class Circle{
+    std::vector<Point> points;
+    double rad = 0;
+
+    Point center;
+    explicit Circle(int_fast32_t x_, int_fast32_t y_) : center(Point(x_, y_)) {
+        points.push_back(center);
+    }
+
+    // метод точки в круге
+
+    // круг по двум точкам
+
+    // круг по 3 точкам
+
+    // основной цикл !!!!
+};
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    std::ios::sync_with_stdio(false), std::cin.tie(0), std::cout.tie(0);
+
+    int_fast32_t n;
+    int_fast32_t x, y;
+
+    std::cin >> n;
+
+    std::cin >> x >> y;
+    Circle circle = Circle(double x, double y);
+
+    for (int_fast32_t i = 1; i < n; i++){
+        std::cin >> x >> y;
+    }
+
+
 }
